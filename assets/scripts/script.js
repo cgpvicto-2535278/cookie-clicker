950d35c70a7e103f251d59ab2c89a63024309528
cgpvicto-2535278 <2535278@etudiant.cegepvicto.ca>
2025-11-28 09:28:56 -0500

    Final

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,16 +6,20 @@ var grosBiscuit = document.getElementById("biscuit");
 var texteBiscuit = document.getElementById("texte-biscuits");
 var boutonClic = document.getElementById("bouton-clic");
 var boutonCurseur = document.getElementById("bouton-curseur");
-var nombreClic = document.getElementById("nombreClic").innerText;
-var nombreCurseur = document.getElementById("nombreCurseurs").innerText;
-var prixCurseur = document.getElementById("prixCurseurs").innerText;
-var prixClic = document.getElementById("prixClic").innerText;
+var nombreClic = document.getElementById("nombreClic");
+var nombreCurseur = document.getElementById("nombreCurseurs");
+var prixCurseur = document.getElementById("prixCurseurs");
+var prixClic = document.getElementById("prixClic");
+var boutonSauve = document.getElementById("bouton-sauvegarde");
+var parSeconde = document.getElementById("par-seconde");
+var parClic = document.getElementById("par-clic");
 
 var biscuits = 0;
 var increment = 1;
 var curseurs = 0;
+var incrementUP = 0;
 
-var biscuitsAuto = setInterval(ajouterBiscuitAuto, 1000/curseurs);
+var biscuitsAuto = setInterval(ajouterBiscuitAuto, 1000);
 
 // ====================================================
 // =  Déclaration des événements                      =
@@ -24,52 +28,51 @@ var biscuitsAuto = setInterval(ajouterBiscuitAuto, 1000/curseurs);
 grosBiscuit.addEventListener('click', ajouterBiscuit);
 boutonClic.addEventListener('click', ameliorerClic);
 boutonCurseur.addEventListener('click', ajouterCurseur);
+boutonSauve.addEventListener('click', sauvegarder);
 
 // ====================================================
 // =  Code qui sera exécuté au chargement de la page  =
 // ====================================================
 
+biscuits = parseInt(localStorage.getItem("biscuits"));
+incrementUP = parseInt(localStorage.getItem("incrementUP"));
+curseurs = parseInt(localStorage.getItem("curseurs"));
 
+refresh();
 
 // ====================================================
 // =  Déclaration des fonctions                       =
 // ====================================================
 
 function ajouterBiscuit(){
-    biscuits += increment;
-    console.log(biscuits);
-    texteBiscuit.innerHTML = biscuits + " biscuits";
-    verifierPrix();
+    biscuits += increment + incrementUP;
+    refresh();
 }
 
 function ajouterBiscuitAuto(){
     if(curseurs != 0){
-        biscuits += increment;
+        biscuits += curseurs;
         //console.log(biscuits);
-        texteBiscuit.innerHTML = biscuits + " biscuits";
+        refresh();
     }
-    verifierPrix();
+    refresh();
 }
 
 function ameliorerClic(){
     if(biscuits >= calculPrixClic()){
         biscuits -= calculPrixClic();
-        increment++;
-        verifierPrix();
-        prixClic = calculPrixClic() + " biscuit(s)";
-        nombreClic = increment;
+        refresh();
+        incrementUP++;
+        refresh();
     }
 }
 
 function ajouterCurseur(){
     if(biscuits >= calculPrixCurseur()){
         biscuits -= calculPrixCurseur();
+        refresh();
         curseurs++;
-        clearInterval(biscuitsAuto);
-        biscuitsAuto = setInterval(ajouterBiscuitAuto, 1000/curseurs);
-        verifierPrix();
-        prixCurseur = calculPrixCurseur() + " biscuit(s)";
-        nombreCurseur = curseurs;
+        refresh();
     }
 }
 
@@ -84,7 +87,7 @@ function calculPrixClic(){
     const base = 200;
     const alpha = 0.25;
     const beta = 1.05;
-    const prix = base * Math.pow(1 + alpha, Math.pow(increment, beta));
+    const prix = base * Math.pow(1 + alpha, Math.pow(incrementUP, beta));
     return Math.round(prix);
 }
 
@@ -99,4 +102,21 @@ function verifierPrix(){
     } else{
         boutonCurseur.style.backgroundColor = "#ccc";
     }
+}
+
+function sauvegarder(){
+    localStorage.setItem("biscuits", biscuits);
+    localStorage.setItem("incrementUP", incrementUP);
+    localStorage.setItem("curseurs", curseurs);
+}
+
+function refresh(){
+    prixClic.innerText = calculPrixClic() + " biscuit(s)";
+    prixCurseur.innerText = calculPrixCurseur() + " biscuit(s)";
+    nombreClic.innerText = incrementUP;
+    nombreCurseur.innerText = curseurs;
+    texteBiscuit.innerText = biscuits + " biscuits";
+    parClic.innerText = increment + incrementUP + " par clic";
+    parSeconde.innerText = curseurs + " par seconde";
+    verifierPrix();
 }
